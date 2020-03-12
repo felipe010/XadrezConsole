9cc02d602ac88fa94af9f30c8003b1fceba1f4bf
Felipe Bontempo <felipe.bontempo@gmail.com>
2020-03-12 16:57:54 -0300

    Criando a classe PosicaoXadrez.cs
    Criei um método que faz a conversão da entrada para uma posição.
    
    Explicação de como é feita a conversão no código, mas basicamente por
    utilizar 'char' na entrada da coluna eu consigo fazer contas, como
    adição por exemplo, usando o número do caractere da tabela ASCII

--- Program.cs
+++ Program.cs
@@ -8,21 +8,11 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8); //Lembre-se quando você instancia uma matriz, ele joga nulo em todo mundo.
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7); //Retorno -> 1, 2 (linha e coluna)
+            /*Seguindo a explicação na classe 'PosicaoXadres.cs' instanciei uma nova posição inserindo os valores 'c' e 7 (coluna e linha), O valor 7 (ref linha) é subtraido de 8 (8 porque a matriz tem uma contagem de 0 ao 7) log o valor da é o resultado 1, E 'c' (minusculo) na tabela ASCII tem o valor de 99, e estou fazendo uma subtração com a letra 'a' que tem valor 97 na tabela, logo o valor vai ser 2 */
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 3));
+            Console.WriteLine(pos.toPosicao());
 
-                Tela.imprimirTabuleiro(tab);
-
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
             Console.ReadLine();
         }
     }
---
+++ xadrez/PosicaoXadrez.cs
@@ -0,0 +1,30 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class PosicaoXadrez
+    {
+        public int linha { get; set; } //Debaixo para cima do 1 ao 8
+        public char coluna { get; set; } //Da esquerda para direita 'a' ao 'h'
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        //Converte a entrada solicitação para posição
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a'); //O macete aqui é justamente usar a númeração da tabela  ASCII referente ao caractere em questão, ex: um tipo char -> 'a' na tabela ascii tem o valor 97 (em decimal), logo se eu tentar somar 'a' + 2 o resultado vai ser -> 99, com isso ao instanciar uma posição ocorre a subtração de qualquer caractere por 97 por eu ter informado a letra 'a'
+        }
+
+        //Para visualização da posição solicitada
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+
+
+    }
+}
